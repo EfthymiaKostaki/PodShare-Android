969ef8d327c6a958bf8c857aef7b4307a90047f1
giorgoskokoro <basketkok@gmail.com>
2020-07-06 20:03:28 +0300

    Create ImageSharedPreference.java

---
+++ app/src/main/java/com/aueb/podshare/Sessions/ImageSharedPreference.java
@@ -0,0 +1,28 @@
+package com.aueb.podshare.Sessions;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class ImageSharedPreference {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    String SHARED_PREF_NAME= "session";
+    String SESSION_KEY = "image";
+
+    public ImageSharedPreference(Context context) {
+        sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void saveSession(String image) {
+        editor.putString(SESSION_KEY, image).commit();
+    }
+
+    public String getSession() {
+        return sharedPreferences.getString(SESSION_KEY, null);
+    }
+
+    public void terminateSession() {
+        editor.putString(SESSION_KEY, null).commit();
+    }
+}
