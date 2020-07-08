1004a95196d21ef1b3e6da202e79886110db2124
EfthymiaKostaki <kostakiefthymia@gmail.com>
2020-07-08 13:20:47 +0300

    Upload podcast image to firebase

--- app/src/main/java/com/aueb/podshare/UploadEpisodeFileActivity.java
+++ app/src/main/java/com/aueb/podshare/UploadEpisodeFileActivity.java
@@ -233,12 +233,10 @@ public class UploadEpisodeFileActivity<StorageReference> extends  AppCompatActiv
                                                 Podcast podcast = new Podcast(podcastNameSharedPreference.getSession(), podcastDescriptionSharedPreference.getSession(), Calendar.getInstance().getTime());
                                                 document.getReference().collection("podcasts").add(podcast);
                                                 Bitmap image = BitmapUtil.decodeBase64(imageSharedPreference.getSession());
-                                                String file_extn = imageSharedPreference.getSession().substring(imageSharedPreference.getSession().lastIndexOf(".") + 1);
-
                                                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
                                                 image.compress(Bitmap.CompressFormat.PNG, 100, baos);
                                                 byte[] data = baos.toByteArray();
-                                                com.google.firebase.storage.StorageReference podcastRef = usersRef.child("users/"+userId+ "/podcasts/"+podcastNameSharedPreference.getSession()+"/"+podcastNameSharedPreference.getSession()+"."+file_extn);
+                                                com.google.firebase.storage.StorageReference podcastRef = usersRef.child("users/" + userId + "/podcasts/" + podcastNameSharedPreference.getSession()+ "/"+ podcastNameSharedPreference.getSession()+".png");
                                                 UploadTask uploadTask = podcastRef.putBytes(data);
                                                 uploadTask.addOnFailureListener(new OnFailureListener() {
                                                     @Override
