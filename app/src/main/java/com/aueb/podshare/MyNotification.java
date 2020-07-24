e9ac9aac2ed8bfc112ae9bdb6ed5e7e0b7d08792
giorgoskokoro <basketkok@gmail.com>
2020-07-24 16:46:14 +0300

    on clear from recent service

--- app/src/main/java/com/aueb/podshare/MyNotification.java
+++ app/src/main/java/com/aueb/podshare/MyNotification.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
@@ -12,11 +13,15 @@ import android.support.v4.media.session.MediaSessionCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.aueb.podshare.Services.NotificationActionService;
 import com.aueb.podshare.classes.Episode;
 
 public class MyNotification {
 
     public static final String CHANNEL_ID = "";
+    public static final String ACTION_PREVIOUS = "action_previous";
+    public static final String ACTION_PLAY = "action_play";
+    public static final String ACTION_NEXT = "action_next";
 
     public static Notification notification;
 
@@ -34,7 +39,29 @@ public class MyNotification {
                 pendingIntentPrevious = null;
                 drw_previous = 0;
             } else {
+                Intent intentPrevious = new Intent(context, NotificationActionService.class)
+                        .setAction(ACTION_PREVIOUS);
+                pendingIntentPrevious = PendingIntent.getBroadcast(context, 0,
+                        intentPrevious, PendingIntent.FLAG_UPDATE_CURRENT);
+                drw_previous = R.drawable.previous;
+            }
 
+            Intent intentPlay = new Intent(context, NotificationActionService.class)
+                    .setAction(ACTION_PLAY);
+            PendingIntent pendingIntentPlay = PendingIntent.getBroadcast(context, 0,
+                    intentPlay, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            PendingIntent pendingIntentNext;
+            int drw_next;
+            if (pos == size) {
+                pendingIntentNext = null;
+                drw_next = 0;
+            } else {
+                Intent intentNext = new Intent(context, NotificationActionService.class)
+                        .setAction(ACTION_NEXT);
+                pendingIntentNext = PendingIntent.getBroadcast(context, 0,
+                        intentNext, PendingIntent.FLAG_UPDATE_CURRENT);
+                drw_next = R.drawable.next;
             }
 
             notification = new NotificationCompat.Builder(context, CHANNEL_ID)
@@ -44,6 +71,12 @@ public class MyNotification {
                     .setLargeIcon(icon)
                     .setOnlyAlertOnce(true)
                     .setShowWhen(false)
+                    .addAction(drw_previous, "previous", pendingIntentPrevious)
+                    .addAction(play_button, "play", pendingIntentPlay)
+                    .addAction(drw_next, "next", pendingIntentNext)
+                    .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                        .setShowActionsInCompactView(0, 1, 2)
+                        .setMediaSession(mediaSessionCompat.getSessionToken()))
                     .setPriority(NotificationCompat.PRIORITY_LOW)
                     .build();
 
---
+++ app/src/main/java/com/aueb/podshare/Services/OnClearFromRecentService.java
@@ -0,0 +1,30 @@
+package com.aueb.podshare.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class OnClearFromRecentService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startID) {
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        stopSelf();
+    }
+}
