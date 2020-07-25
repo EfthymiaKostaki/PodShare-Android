abffe128d300853408c1693d6c0a354ce93df161
theodorapapako <44336512+theodorapapako@users.noreply.github.com>
2020-07-25 18:00:17 +0300

    Change episode through player page

--- app/src/main/java/com/aueb/podshare/MyMediaPlayerFragment.java
+++ app/src/main/java/com/aueb/podshare/MyMediaPlayerFragment.java
@@ -2,6 +2,7 @@ package com.aueb.podshare;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -27,7 +28,9 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -42,6 +45,10 @@ import com.aueb.podshare.classes.Episode;
 import com.aueb.podshare.classes.Podcast;
 import com.aueb.podshare.classes.User;
 import com.aueb.podshare.utils.BitmapUtil;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,12 +60,14 @@ public class MyMediaPlayerFragment extends Fragment implements Playable {
     int position = 0;
     boolean isPlaying = false;
     TextView elapsedTime;
+    private ProgressDialog progressDialog;
     TextView remainingTime;
     SeekBar seekBar;
     Runnable runnable;
     MediaPlayer mediaPlayer;
     NotificationManager notificationManager;
     private Button playButton;
+
     BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -81,6 +90,7 @@ public class MyMediaPlayerFragment extends Fragment implements Playable {
             }
         }
     };
+
     private ListView episodesList;
     private ArrayList<Episode> podcastEpisodes = new ArrayList<>();
     private ArrayList<String> episodeTitles = new ArrayList<>();
@@ -89,6 +99,7 @@ public class MyMediaPlayerFragment extends Fragment implements Playable {
     private ArrayList<Uri> episodesAudioUri;
     private Bitmap audioPlay;
     private int indexCurrentAudioPlaying;
+
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -125,6 +136,8 @@ public class MyMediaPlayerFragment extends Fragment implements Playable {
         episodeText.setText(episode.getSession());
         TextView posharerText = view.findViewById(R.id.podsharer_play_text);
         posharerText.setText(podsharer.getSession());
+        TextView podcastText = view.findViewById(R.id.podcast_name_play);
+        podcastText.setText(podcast.getSession());
         TextView descriptionEpisode = view.findViewById(R.id.description_play);
         descriptionEpisode.setText(episodeDescription.getSession());
         Bitmap bitmap = BitmapFactory.decodeByteArray(podcastImage, 0, podcastImage.length);
@@ -228,11 +241,70 @@ public class MyMediaPlayerFragment extends Fragment implements Playable {
 
         episodesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // The selected episode will start playing
+                String selected = ((TextView) view.findViewById(R.id.episode_name_txt)).getText().toString();
+
+                Toast toast = Toast.makeText(getContext(), selected, Toast.LENGTH_SHORT);
+                toast.show();
+                EpisodeNameSharedPreference episode = new EpisodeNameSharedPreference(getContext());
+                EpisodeDescriptionSharedPreference description = new EpisodeDescriptionSharedPreference(getContext());
+                String descriptionEp = ((TextView) view.findViewById(R.id.episode_description)).getText().toString();
+                description.saveSession(descriptionEp);
+                episode.saveSession(selected);
+                showLoading();
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageRef = storage.getReference();
+                final PodcastNameSharedPreference podcast = new PodcastNameSharedPreference(getContext());
+                int index = 0;
+                final int[] countDownloadedUris = {0};
+                ArrayList<Episode> episodes = getPodcast().getEpisodes();
+                ArrayList<Uri> uris = new ArrayList<>();
+                for (final int[] i = {0}; i[0] < episodes.size(); i[0]++) {
+                    Log.d(TAG, "Inside for: " + i[0]);
+                    if (episode.getSession().equals(episodes.get(i[0]).get_name())) {
+                        index = i[0];
+                        Log.d(TAG, "Found episode selected index: " + i[0]);
+                    }
+
+                    StorageReference episodesRef = storageRef.child("users/" + user.getUid() + "/podcasts/" + podcast.getSession() + "/episodes/" + episodes.get(i[0]).get_name() + "/" + episodes.get(i[0]).get_name() + ".mp3");
+                    int finalIndex = index;
+                    episodesRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            // Got the download URL for 'users/me/profile.png'
+                            uris.add(uri);
+                            Log.d(TAG, "Adding uri in position: " + finalIndex);
+                            if (countDownloadedUris[0] == episodes.size() - 1) {
+                                dismissLoading();
+                                loadFragment(new MyMediaPlayerFragment(user, podcastImage, uris, finalIndex));
+                            }
+                            countDownloadedUris[0]++;
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle any errors
+                            Log.d(TAG, "Failure in index: " + finalIndex);
+                        }
+                    });
+                }
+
             }
         });
 
         return view;
+    }
+
+    private Podcast getPodcast() {
+        PodcastNameSharedPreference podcastSession = new PodcastNameSharedPreference(getContext());
+        String podcastName = podcastSession.getSession();
+        ArrayList<Podcast> podcasts = user.getPodcasts();
+        Podcast podcast = null;
+        for (Podcast pod : podcasts) {
+            if (pod.getName().equals(podcastName)) {
+                podcast = pod;
+            }
+        }
+        return podcast;
     }
 
     private void createChannel() {
@@ -340,5 +412,21 @@ public class MyMediaPlayerFragment extends Fragment implements Playable {
         transaction.addToBackStack(null);
         transaction.commit();
     }
+
+    private void showLoading() {
+        if (progressDialog == null)
+            progressDialog = new ProgressDialog(getContext());
+
+        progressDialog.setMessage(getString(R.string.fetching_episodes));
+        progressDialog.setCancelable(false);
+        progressDialog.setIndeterminate(true);
+        progressDialog.show();
+    }
+
+    private void dismissLoading() {
+        if (progressDialog != null)
+            progressDialog.dismiss();
+    }
+
 
 }
